1f08ecb1c361002e23c36fd7e6a6a22934e36d1a
nyakuOHO <tea.zhen900708@gmail.com>
2017-02-27 15:47:34 +0900

    First short

---
+++ app/src/main/java/sakr/toast/ToastOHO.java
@@ -0,0 +1,28 @@
+package sakr.toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ToastOHO extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toast_oho);
+        Button toastbutton = (Button) findViewById(R.id.PUSH);
+
+        toastbutton.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(view.getContext(), "聽說這個顯示時間比較短(´・ω・｀)", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
